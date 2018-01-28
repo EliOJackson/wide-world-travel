0a5366ad1523f0b769cee5275de07fdf287b9ad3
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-01-28 12:55:34 -0600

    moved json to a data folder, updated partial to make html more clear

--- dist/project.js
+++ dist/project.js
@@ -851,3 +851,63 @@
 }) (angular.module ('wideWorldTravel', ['ngRoute']));
 
 
+
+//====================================================================================================================
+// Module:    wideWorldTravel
+// Optimized: Yes
+// File:      ./app/app.js
+//====================================================================================================================
+
+(function (module) {
+
+  'use strict';
+
+  module
+      .config($routeProvider => {
+          $routeProvider
+              .when("/", {
+                  templateUrl: "partials/books.html",
+                  controller: "GuideCtrl"
+              })
+              .otherwise("/");
+      });
+
+//--------------------------------------------------------------------------------------------------------------------
+// File: ./app/controllers/GuideCtrl.js
+//--------------------------------------------------------------------------------------------------------------------
+
+  'use strict';
+
+  module.controller("GuideCtrl", function ($scope, GuideFactory) {
+      GuideFactory.getGuides()
+          .then((guideData) => {
+              $scope.allGuides = guideData.data.guides;
+
+          });
+  });
+//--------------------------------------------------------------------------------------------------------------------
+// File: ./app/factories/GuideFactory.js
+//--------------------------------------------------------------------------------------------------------------------
+
+  'use strict';
+
+  module.factory("GuideFactory", function ($q, $http) {
+
+      let getGuides = () => {
+          return $q((resolve, reject) => {
+              $http
+                  .get("../data/guides.json")
+                  .then((guides) => {
+                      resolve(guides);
+                  })
+                  .catch((err) => {
+                      reject(err);
+                  });
+          });
+      };
+      return { getGuides };
+  });
+
+}) (angular.module ('wideWorldTravel', ['ngRoute']));
+
+
